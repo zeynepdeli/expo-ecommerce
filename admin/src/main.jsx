8096b7ed1fe4fb027be4f506ed5eb59f70151dee
zeynepdeli <zeynepdeli_27@outlook.com>
2026-01-19 15:00:27 +0300

    auth implemented

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+//Import your Publishable Key
+const PUBLISHABLE_KEY=import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if(!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <App />
+    </ClerkProvider>
   </StrictMode>,
 )
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose"
+
+const addressesSchema = new mongoose.Schema({
+    label: {
+        type:String,
+        required:true
+    },
+    fullName:{
+        type:String,
+        required:true
+    },
+    streetAddress:{
+        type:String,
+        required:true
+    },
+    city: {
+        type:String,
+        required:true
+    },
+    state: {
+        type:String,
+        required:true
+    },
+    zipCode: {
+        type:String,
+        required:true
+    },
+    phoneNumber: {
+        type:String,
+        required:true
+    },
+    isDefault: {
+        type:Boolean,
+        default:false
+    }
+})
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    imageUrl:{
+        type:String,
+        default:""
+    },
+    clerkId: {
+        type:String,
+        unique:true,
+        required:true,
+    },
+    adresses:[addressesSchema],
+
+    wishlist:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Product"
+        }
+    ]
+},{timestamps:true})
+
+export const User = mongoose.model("User",userSchema)
