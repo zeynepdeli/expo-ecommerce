76958761d63e65c60fd53a0a727cb678fa103732
zeynepdeli <zeynepdeli_27@outlook.com>
2026-01-22 20:22:29 +0300

    review & product routes & controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,9 @@ import {functions,inngest} from  "./config/inngest.js"
 import adminRoutes from "./routes/admin.route.js"
 import userRoutes from "./routes/user.route.js"
 import orderRoutes from "./routes/order.route.js"
+import reviewRoutes from "./routes/review.route.js"
+import productRoutes from "./routes/product.route.js"
+
 
 
 
@@ -24,7 +27,11 @@ app.use("/api/inngest", serve({client:inngest, functions }))
 
 app.use("/api/admin",adminRoutes)
 app.use("/api/users",userRoutes)
-app.use("/api/order",orderRoutes)
+app.use("/api/orders",orderRoutes)
+app.use("/api/reviews",reviewRoutes)
+app.use("/api/products",productRoutes)
+
+
 
 
 
