f0354b0022a3a73b85ebf6c4933e53bded992984
zeynepdeli <zeynepdeli_27@outlook.com>
2026-01-23 00:23:20 +0300

    cart route & controllers cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,9 @@ import userRoutes from "./routes/user.route.js"
 import orderRoutes from "./routes/order.route.js"
 import reviewRoutes from "./routes/review.route.js"
 import productRoutes from "./routes/product.route.js"
+import cartRoutes from "./routes/cart.route.js"
+import cors from "cors"
+
 
 
 
@@ -22,6 +25,7 @@ const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(clerkMiddleware()) // adds auth object under the req => req.auth
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}))// credentials: true allows the browser to send the cookies to the server with the request
 
 app.use("/api/inngest", serve({client:inngest, functions }))
 
@@ -30,6 +34,8 @@ app.use("/api/users",userRoutes)
 app.use("/api/orders",orderRoutes)
 app.use("/api/reviews",reviewRoutes)
 app.use("/api/products",productRoutes)
+app.use("/api/cart",cartRoutes)
+
 
 
 
